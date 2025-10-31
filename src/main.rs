88a96b0d4bf3a5b213d403d9594afaf2f6ce8c11
ImLuCz <lucabrambilla3010@gmail.com>
2025-10-31 11:10:57 +0100

    feat: add hello world text

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![no_std]
-#![no_main]
+#![no_main] // Disables language-level entry points
 
 use core::panic::PanicInfo;
 
@@ -8,6 +8,8 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+static HELLO_WORLD: &[u8] = b"Hello World!";
 
 /*
 Disable name mangling so the compiler actually outputs a function with the name _start and not
@@ -24,8 +26,17 @@ Instead of Rust's because it's unspecified w/o std library
 The function is diverging because the entry point is not called by any function, but by the os or bootloader.
 Instead of returning, it should invoke the exit syscall (for example)
 
-TL;DR _start = main
+TLDR _start = main
 */
 pub extern "C" fn _start() -> ! {
+    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO_WORLD.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // 0xb = light cyan
+        }
+    }
+
     loop {}
 }
